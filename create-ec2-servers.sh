31ba46209b872f854d5df740e4953a5ddbd7bdb0
deepak <18l11a0518@gmail.com>
2023-05-07 12:35:49 +0530

    created 11 servers

---
+++ create-ec2-servers.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -x
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-8-DevOps-Practice" | jq '.Images[].ImageId' | xargs)
+SG_NAME="allow-all"
+SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${SG_NAME} | jq  '.SecurityGroups[].GroupId' | xargs)
+if [ -z "${SGID}" ]; then
+  echo "Given Security Group does not exit"
+  exit 1
+fi
+
+aws ec2 request-spot-instances \
+  --instance-count=11 \
+  --image-id ${AMI_ID} \
+  --tag-specifications "ResourceType=spot-instance-request,Tags=[Key=Name, Value=${COMPONENT}]" \
+  --instance-type=t3.micro \
+  --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
+  --security-group-ids="${SGID}"
+
+for component in frontend mongodb catalogue cart mysql user payment shipping dispatch redis rabbitmq; do
+  COMPONENT="${component}"
+done
