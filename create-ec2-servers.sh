72368855701f817eabc51d6d897724066b3ec449
deepak <18l11a0518@gmail.com>
2023-05-13 16:54:53 +0530

    script updated

--- create-ec2-servers.sh
+++ create-ec2-servers.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 set -x
-AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-8-DevOps-Practice" | jq '.Images[].ImageId' | xargs)
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-8-DevOps-Practice" | jq -r '.Images[].ImageId')
 SG_NAME="allow-all"
-SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${SG_NAME} | jq  '.SecurityGroups[].GroupId' | xargs)
+SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${SG_NAME} | jq -r '.SecurityGroups[].GroupId')
+
 if [ -z "${SGID}" ]; then
-  echo "Given Security Group does not exit"
+  echo "Given Security Group does not exist"
   exit 1
 fi
 
@@ -13,6 +14,11 @@ for component in frontend mongodb catalogue cart mysql user payment shipping dis
   COMPONENT="${component}"
   aws ec2 request-spot-instances \
     --instance-count 1 \
-    --launch-specification "{\"ImageId\":\"${AMI_ID}\",\"InstanceType\":\"t3.micro\",\"SecurityGroupIds\":[\"${SGID}\"],\"TagSpecifications\":[{\"ResourceType\":\"spot-instances-request\",\"Tags\":[{\"Key\":\"Name\",\"Value\":\"${COMPONENT}\"}]}],\"InstanceMarketOptions\":{\"MarketType\":\"spot\",\"SpotOptions\":{\"SpotInstanceType\":\"persistent\",\"InstanceInterruptionBehavior\":\"stop\"}}}"
+    --image-id "${AMI_ID}" \
+    --instance-type t3.micro \
+    --instance-market-option "MarketType=spot,SpotOptions={InstanceInterruptionBehavior=stop,SpotInstanceType=persistent}" \
+    --security-group-ids "${SGID}" \
+    --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]"
 done
+
 
